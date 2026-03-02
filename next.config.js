163caae97bfe1b6a612af5c6049739339a979e3d
Andrew <andrew@krak.com>
2026-03-02 22:29:14 +0800

    Remove standalone output - use default Vercel deployment

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
-  // Reduce memory usage during build
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client'],
-  },
-  // Static export for simpler deployment
-  output: 'standalone',
 }
 
 module.exports = nextConfig
